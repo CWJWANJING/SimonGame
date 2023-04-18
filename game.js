fa761745fde34b4ff99568d04326c76763b2273c
Wanjing Chen <wanjing.chen@thoughtworks.com>
2023-04-18 19:26:43 +0100

    Added comments

--- game.js
+++ game.js
@@ -8,101 +8,104 @@ gameStarted = false;
 
 level = 0;
 
-$(".btn").click(function() {
-    if (gameStarted == true) {
-        userClickedColour = $(this).attr("id");
-        playSoundByColour(userClickedColour);
-        blink("#"+userClickedColour, 1);
-        userClickedPattern.push(userClickedColour);
-        level = checkPattern(userClickedPattern.length-1);
-    } 
-})
-
-$("body").on("keydown", function () {
-    delay = 150; // 0.15 second
-    if (gameStarted == false) {
-        setTimeout(function() {
-            gameStarted = true;
-            nextSequence();
-            $("h1").text("Level " + level);
-        }, delay);
-    }
-  });
-
 buttonColours = ["red", "green", "yellow", "blue"];
 
+$(".btn").click(function () {
+  if (gameStarted == true) {
+    userClickedColour = $(this).attr("id");
+    // click button animation
+    playSoundByColour(userClickedColour);
+    blink("#" + userClickedColour, 1);
+
+    userClickedPattern.push(userClickedColour);
+    level = checkPattern(userClickedPattern.length - 1);
+  }
+});
+
+$("body").on("keydown", function () {
+  delay = 150; // 0.15 second
+  if (gameStarted == false) {
+    setTimeout(function () {
+      gameStarted = true;
+      nextSequence();
+      $("h1").text("Level " + level);
+    }, delay);
+  }
+});
+
 function checkPattern(level) {
-    delay = 700;
+  delay = 700; // 0.7 seconds
 
-    if (userClickedPattern[level] === gameSequence[level]) {
-        if (userClickedPattern.length === gameSequence.length) {
-            setTimeout(function() {
-                level++;
-                $("h1").text("Level " + level);
-                userClickedPattern = [];
-                nextSequence();
-            }, delay);
-        } 
-    } else {
-
-        gameOver();
-
-        return 0;
+  if (userClickedPattern[level] === gameSequence[level]) {
+    if (userClickedPattern.length === gameSequence.length) {
+      setTimeout(function () {
+        level++;
+        $("h1").text("Level " + level);
+        userClickedPattern = [];
+        nextSequence();
+      }, delay);
     }
+  } else {
+    gameOver();
 
-    return level;
+    return 0;
+  }
+
+  return level;
 }
 
 function gameOver() {
-    delay = 250
-    playSound("sounds/wrong.mp3");
-    $("h1").text("Game over, press any key to restart");
-    $("body").addClass("game-over");
-    setTimeout(function() {
-        $("body").removeClass("game-over");
-    }, 250);
+  delay = 250;
+  // game over UI and sound effects
+  playSound("sounds/wrong.mp3");
+  $("h1").text("Game over, press any key to restart");
+  $("body").addClass("game-over");
+  setTimeout(function () {
+    $("body").removeClass("game-over");
+  }, 250);
 
-    restart();
+  restart();
 }
 
 function restart() {
-    gameSequence = [];
-    userClickedPattern = [];
-    gameStarted = false;
+  gameSequence = [];
+  userClickedPattern = [];
+  gameStarted = false;
 }
 
 function nextSequence() {
-    range = 4;
-    randomNumber = Math.floor(Math.random() * range);
-    randomColour = buttonColours[randomNumber];
-    blink("#"+randomColour, 1);
-    playSoundByColour(randomColour);
-    gameSequence.push(randomColour);
+  range = 4;
+  // the next sequence is randomly picked
+  randomNumber = Math.floor(Math.random() * range);
+  randomColour = buttonColours[randomNumber];
+  blink("#" + randomColour, 1);
+  playSoundByColour(randomColour);
+  gameSequence.push(randomColour);
 }
 
 function blink(colourBtn, repeat) {
-    if(!repeat) return;
-    $(colourBtn).fadeOut(150, function() {
-        $(this).fadeIn(150, function() {
-            blink(this, repeat - 1);
-        });
+  if (!repeat) return;
+  $(colourBtn).fadeOut(150, function () {
+    $(this).fadeIn(150, function () {
+      blink(this, repeat - 1);
     });
+  });
 }
 
 function playSoundByColour(colour) {
-    switch(colour) {
-        case "red":
-            playSound("sounds/red.mp3");
-        case "green":
-            playSound("sounds/green.mp3");
-        case "yellow":
-            playSound("sounds/yellow.mp3");
-        case "blue":
-            playSound("sounds/blue.mp3");
-    }
+  switch (colour) {
+    case "red":
+      playSound("sounds/red.mp3");
+    case "green":
+      playSound("sounds/green.mp3");
+    case "yellow":
+      playSound("sounds/yellow.mp3");
+    case "blue":
+      playSound("sounds/blue.mp3");
+  }
 }
 
-function playSound (audioPath) {
-    audio = new Audio(audioPath);
-    audio.play();
+function playSound(audioPath) {
+  audio = new Audio(audioPath);
+  audio.play();
 }
